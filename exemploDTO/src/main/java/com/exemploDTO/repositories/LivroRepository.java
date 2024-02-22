75e66e5a0d62633ab9db43b436094213b4df4e19
Rafalanches <133991142+Rafalanches@users.noreply.github.com>
2024-02-22 15:56:29 -0300

    Delete exemploDTO directory

--- exemploDTO/src/main/java/com/exemploDTO/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemploDTO.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exemploDTO.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro,Long>{
-
-}
